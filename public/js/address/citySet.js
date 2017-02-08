9890f18d250833e2f829970e284406e11b62ec84
yingyingl <lyy541066368@126.com>
2017-02-08 12:19:51 +0800

    master

---
+++ public/js/address/citySet.js
@@ -0,0 +1,152 @@
+﻿var ths;
+function SelCity(obj, e) {
+    ths = obj;
+    //var dal = '<div class="_citys"><span title="关闭" id="cColse" >×</span><ul id="_citysheng" class="_citys0"><li class="citySel">省份</li><li>城市</li><li>区县</li></ul><div id="_citys0" class="_citys1"></div><div style="display:none" id="_citys1" class="_citys1"></div><div style="display:none" id="_citys2" class="_citys1"></div></div>';
+    var dal = '<div class="_citys"><span title="关闭" id="cColse" >×</span><ul id="_citysheng" class="_citys0"><li class="citySel">省份</li><li>城市</li></ul><div id="_citys0" class="_citys1"></div><div style="display:none" id="_citys1" class="_citys1"></div></div>';
+    Iput.show({id: ths, event: e, content: dal, width: "470"});
+    $('#cColse').click(function () {
+        Iput.colse();
+    });
+
+    $('#_citys0').append(Iput.getProv());
+
+    provEvent();
+
+    $('#_citysheng li').click(function () {
+        $('#_citysheng li').removeClass('citySel');
+        $(this).addClass('citySel');
+        var s = $('#_citysheng li').index(this);
+        $('._citys1').hide();
+        $('._citys1:eq(' + s + ')').show();
+    });
+    Iput.autoCheck(obj.value, cityEvent);
+}
+
+function provEvent () {
+    $('#_citys0 a').click(function () {
+        $("#_citysheng li").removeClass("citySel");     //点击省之后，将市的tab选中，省的tab取消选中
+        $("#_citysheng li:eq(1)").addClass("citySel");
+        $('#_citys1 a').remove();   // 删除旧的市的数据
+        $('#_citys1').append(Iput.getCity(this));   // 添加新的市数据
+        $('._citys1').hide();   // 隐藏省的数据
+        $('._citys1:eq(1)').show(); // 显示市的数据
+        $('#_citys0 a,#_citys1 a,#_citys2 a').removeClass('AreaS');
+        $(this).addClass('AreaS');
+        var lev = $(this).data('name');
+        ths.value = lev;
+        if ($('#' + ths.dataset.prefix + '_prov').length == 0) {
+            var hcitys = $('<input>', {
+                type: 'hidden',
+                name: 'prov',
+                'data-name': lev,
+                id: ths.dataset.prefix + '_prov',
+                val: $(this).data('id')
+            });
+            $(ths).after(hcitys);
+        }
+        else {
+            $('#' + ths.dataset.prefix + '_prov').val($(this).data('id')).data('name', lev);
+        }
+        cityEvent();
+    });
+}
+
+function cityEvent () {
+    $("#_citys1 a").click(function () {
+        $('#_citys1 a,#_citys2 a').removeClass('AreaS');
+        $(this).addClass('AreaS');
+        var lev = $(this).data('name');
+        if ($('#' + ths.dataset.prefix + '_city').length == 0) {
+            var hcitys = $('<input>', {
+                type: 'hidden',
+                name: 'city',
+                'data-name': lev,
+                id: ths.dataset.prefix + '_city',
+                val: $(this).data('id')
+            });
+            $(ths).after(hcitys);
+        }
+        else {
+            $('#' + ths.dataset.prefix + '_city').val($(this).data('id')).data('name', lev);
+        }
+        var bc = $('#' + ths.dataset.prefix + '_prov').data('name');
+        ths.value = bc + Iput.confg.separator + $(this).data('name');
+
+        Iput.colse();
+
+        //var ar = getArea($(this));
+        //
+        //$("#_citys2 a").remove();
+        //$("#_citys2").append(ar);
+        //$("._citys1").hide();
+        //$("._citys1:eq(2)").show();
+
+    });
+}
+
+
+function areaEvent () {
+
+    //$("#_citys2 a").click(function () {
+    //    $("#_citys2 a").removeClass("AreaS");
+    //    $(this).addClass("AreaS");
+    //    var lev = $(this).data("name");
+    //    if (document.getElementById("harea") == null) {
+    //        var hcitys = $('<input>', {
+    //            type: 'hidden',
+    //            name: "harea",
+    //            "data-name": lev,
+    //            id: "harea",
+    //            val: $(this).data("id")
+    //        });
+    //        $(ths).after(hcitys);
+    //    }
+    //    else {
+    //        $("#harea").val(lev);
+    //        $("#harea").attr("data-id", $(this).data("id"));
+    //    }
+    //    var bc = $("#hcity").data('name');
+    //    var bp = $("#hproper").data('name');
+    //    ths.value = bc + Iput.confg.separator + bp + Iput.confg.separator + $(this).data("name");
+    //    Iput.colse();
+    //});
+}
+
+function getArea(obj) {
+    var c = obj.data('id');
+    var e = area;
+    var f = [];
+    var g = '';
+    for (var i = 0, plen = e.length; i < plen; i++) {
+        if (e[i]['pid'] == parseInt(c)) {
+            f.push(e[i]);
+        }
+    }
+    for (var j = 0, clen = f.length; j < clen; j++) {
+        g += '<a data-level="1" data-id="' + f[j]['id'] + '" data-name="' + f[j]['name'] + '" title="' + f[j]['name'] + '">' + f[j]['name'] + '</a>'
+    }
+
+    $("#_citysheng li").removeClass("citySel");
+    $("#_citysheng li:eq(2)").addClass("citySel");
+    return g;
+}
+
+// *****************************自此添加功能代码*********************************
+
+function destroy(prefix) {
+    $('#PREFIX_city,#PREFIX_prov'.replace(/PREFIX/g, prefix)).remove();
+}
+
+function setDefault(obj, val) {
+    if (val != '') {
+        var data = val.split(Iput.confg.separator);
+
+        if (data.length != 2) {
+            console.log('data source error ......');
+        } else {
+            obj.click();
+            $('#_citys0 a[data-name=' + data.shift() + ']').click();
+            $('#_citys1 a[data-name=' + data + ']').click();
+        }
+    }
+}
---
+++ public/js/group/group.js
@@ -0,0 +1,158 @@
+//VM:54mI5pys57yW5Y+377yaMi4yLjgNCuS/ruaUueaXtumXtO+8mjIwMTYvMDcvMDYNCuS/ruaUueS6uuWRmO+8muWImOa1qQ0K5L+u5pS55YaF5a6577yaQlVHIzEwNTEg44CQ5o6o6YCB5Yqg5Lq644CR5a+55bey5aSx5pWI55qE5oiQ5ZGY6YCa6L+H5omr56CB5YaN5q2k55Sz6K+36K+l5pa55qGI5pe277yM5Zyo5pyq6YCa6L+H5a6h5qC455qE5oOF5Ya15LiL77yMR1VM6KGo55qEc3ViamVjdF9pZOS4ujANCg==
+
+
+function checkBirthCertExist(code,root){
+    var flag=null;
+    callAjax({'code': code.toUpperCase()}, 0, function (response) {
+        flag=response;
+    }, root+'/check/checkBirthCertExist/');
+    return flag;
+}
+
+/*
+ * 检查出生证的格式
+ * @param code--出生证号
+ * @return 0：非法； 1：合法
+ * */
+function checkBirthCertFormat(code){
+    var result=0;
+    if(/^[a-zA-Z][0-9]{9}$/.test(code)){
+        result=1;
+    }
+    return result;
+}
+
+/*
+ *因为年龄限制只能卡到年。如果是出生证，小于1年的情况需要单独判断
+ * @param birthDayStr: 出生日期的字符串，如'1986-07-27'
+ * @param type: 限制类型-/日（1）/月(2)/年(3)
+ * @param num: 限制数字-与type组合进行验证
+ * */
+function checkBirthDateValid(birthDayStr,type,num){
+    var result=0;
+    var birthDate=new Date(birthDayStr);
+    var todayDate=new Date();
+    var wholeDiffDays=diffDays(todayDate, birthDate);
+    if(type==1 && wholeDiffDays>=num){
+        result=1;
+    }else{
+        var todayYear=todayDate.getFullYear();
+        var birthYear=birthDate.getFullYear();
+        var todayMonth=todayDate.getMonth() + 1;
+        var birthMonth=birthDate.getMonth() + 1;
+        var todayDay=todayDate.getDate();
+        var birthDay=birthDate.getDate();
+        var gapYear=todayYear-birthYear;
+        var gapMonth=todayMonth-birthMonth;
+        if(type==2){
+            if(gapYear>0){
+                result=1;
+            }else if(gapYear==0){
+                if(gapMonth>num){
+                    result=1;
+                }else if(gapMonth==num){
+                    if(todayDay>=birthDay){
+                        result=1;
+                    }
+                }
+            }
+        }else if(type==3){
+            if(gapYear>num){
+                result=1;
+            }else if(gapYear==num){
+                if(gapMonth>0){
+                    result=1;
+                }else if(gapMonth==0){
+                    if(todayDay>=birthDay){
+                        result=1;
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+/*
+* 检查扫码加人的主被保险人的用户名/身份证号
+* @param account: 主被保险人的用户名/身份证号码
+* @return -1：空 0: 非法; 1: username; 2: id_number
+* */
+function checkGroupRegMainAccount(account){
+    var result=0;
+    if(account.length==0){
+        result=-1;
+    }
+    else if(check_username_format(account,false)==1){
+        result=1;
+    }else if(check_id_number_format(account)){
+        result=2;
+    }
+    return result;
+}
+/*检查是否达到最大人数
+ * @param eid
+ * @param groupId
+ * @param root-__WWW__
+ * @return bool
+ * */
+function checkMaxPersonNumber(eid, groupId,root) {
+    var flag = false;
+    callAjax({'eid': eid, 'group_id': groupId}, 0, function (response) {
+        if (response == 1) {
+            flag = true;
+        }
+    }, root+'/groupreg/checkMaxPersonNumber/');
+    return flag;
+}
+/*
+ * 根据关系类型和证件类型检查扫码加人的连带被保险人的账号
+ * @param account: 连带被保险人的账号
+ * @param relType：关系类型 - 2：子女（目前暂时只实现这个）
+ * @param certType: 证件类型 -0：身份证；1：出生证
+ * @return 0: 非法; 1: 合法
+ * */
+function checkRelativeAccount(relType,certType,account){
+    var result=0;
+    if(account.length==0){
+        result=-1;
+    }else if(relType == 2){
+        if(certType==0 && check_id_number_format(account)){
+            result=1;
+        }else if(certType==1 && checkBirthCertFormat(account)==1){
+            result=1;
+        }
+    }
+    return result;
+}
+
+/*检查被保险人是否可以申请保障
+* @param relType：申请人的关系类型- 1：本人；2：子女
+* @param groupId
+* @param eid
+* @param toApplyId:证件号码
+* @param birthDay：生日xxxx-xx-xx
+* @param starttime:开始日期
+* @param mainId:主被保险人证件号码
+* @param root-__WWW__
+* @return bool
+* */
+function checkUserCanApplyIns(relType,groupId,eid,toApplyId,birthDay,starttime,mainId,root) {
+    var flag=false;
+    callAjax({'relType': relType, 'groupId': groupId,'eid':eid,'toApplyId':toApplyId,'birthDay':birthDay,'starttime':starttime,'mainCertCode':mainId}, 0, function (response) {
+        flag=response;
+    }, root+'/Groupreg/checkUserCanApplyIns/');
+    return flag;
+}
+
+/*
+* 计算两个日期的间隔天数（第一天不算）
+* @param startDate 开始日期-Date()对象
+* @param endDate 结束日期-Date()对象
+* */
+function diffDays(startDate, endDate)
+{
+    var millisecondsPerDay=1000 * 60 * 60 * 24;
+    var millisBetween=startDate.getTime() - endDate.getTime();
+    var days=Math.floor(millisBetween / millisecondsPerDay);
+    return days;
+}
